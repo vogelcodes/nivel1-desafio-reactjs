450244121952ea267e569c19ee7aca1dc1b11d46
Daniel Vogel <danielcvt@gmail.com>
2020-04-29 04:52:35 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from 'services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(res=>{
+      setRepositories(res.data);
+    });
+  }, [repositories]);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('/repositories', {title: 'Teste React', url: 'http:localhost//', techs: ['reactjs']}).then(res=>{
+        repositories.push(res.data);
+        setRepositories(repositories);
+      
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`);
+    api.get('/repositories').then(res=>{
+      setRepositories(res.data);
+    });
   }
-
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => 
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <li key={repo.id}>
+          {repo.title}
+
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+        
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
---
+++ src/components/Repo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Repo(props) {
+  return (
+    <li>props.name</li><
+  );
+}
